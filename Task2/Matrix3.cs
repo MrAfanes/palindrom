2ba7c27db0203ff65ab40670ea2bf2c1796c9438
Дмитрий <mr.afanes@gmail.com>
2021-09-04 10:55:05 +0500

    1

---
+++ Task2/Matrix3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Matrix3
+    {
+        public static int determinant3()
+        {
+            Console.WriteLine("Заполните матрицу 3х3");
+            int[,,] matrix = new int[3, 3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        matrix[i, j, k] = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+            int det3 = matrix[0, 0, 0] * matrix[1, 1, 1] * matrix[2, 2, 2] + matrix[0, 1] * matrix[1, 0];
+            return det3;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите количество столбцов/строк от 1 до 3");
+            int n = int.Parse(Console.ReadLine());
+            if(n == 1)
+            {
+                Console.WriteLine("Введите число");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Определитель 1-го порядка - " + x);
+            }
+            else if(n == 2)
+            {
+                int x = Task2.Matrix2.determinant2();
+                Console.WriteLine("Определитель 2-го порядка - " + x);
+            }
+            else if(n == 3)
+            {
+                int x = Task2.Matrix3.determinant3();
+                Console.WriteLine("Определитель 3-го порядка - " + x);
+            }
+        }
+    }
