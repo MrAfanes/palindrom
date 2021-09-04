dd4da2c77af5c430afd1813cecd5f35760fd4e3a
Дмитрий <mr.afanes@gmail.com>
2021-09-04 14:11:23 +0500

    Time

--- Time/Program.cs
+++ Time/Program.cs
@@ -2,11 +2,43 @@
 
 namespace Time
 {
-    class Program
+    class Time
     {
+
+        //static int hours = 0;
+        //static int minutes = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int hours = 0;
+            int minutes = 0;
+            Time time = new Time(hours, minutes);
+            time.setTime();
+            time.getTime();
+        }
+
+
+        public Time(int hours, int minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+        public void setTime()
+        {
+            Console.WriteLine("Введите колличество часов");
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите колличество минут");
+            int m = int.Parse(Console.ReadLine());
+            h = h + hours;
+            m = m + minutes;
+            if(m > 59)
+            {
+                hours++;
+                minutes = m - 59;
+            }
+        }
+        public int getTime()
+        {
+            return 0;
         }
     }
 }
