764d908d827633d2bcad7aa650159190f9ea9e5e
Дмитрий <mr.afanes@gmail.com>
2021-09-04 12:10:28 +0500

    65651

--- Task2/Matrix3.cs
+++ Task2/Matrix3.cs
@@ -11,18 +11,15 @@ namespace Task2
         public static int determinant3()
         {
             Console.WriteLine("Заполните матрицу 3х3");
-            int[,,] matrix = new int[3, 3, 3];
+            int[,] matrix = new int[3, 3];
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    for (int k = 0; k < 3; k++)
-                    {
-                        matrix[i, j, k] = int.Parse(Console.ReadLine());
-                    }
+                        matrix[i, j] = int.Parse(Console.ReadLine());
                 }
             }
-            int det3 = matrix[0, 0, 0] * matrix[1, 1, 1] * matrix[2, 2, 2] + matrix[0, 1] * matrix[1, 0];
+            int det3 = matrix[0, 0] * matrix[1, 1] * matrix[2, 2] + matrix[0, 1] * matrix[1, 2] * matrix[2, 0] + matrix[1, 0] * matrix[2, 1] * matrix[0, 2] - matrix[0, 2] * matrix[1, 1] * matrix[2, 0] - matrix[0, 1] * matrix[1, 0] * matrix[2, 2] - matrix[2, 1] * matrix[1, 2] * matrix[0, 0];
             return det3;
         }
     }
